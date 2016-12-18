51c03c6171cceef9bab3dd5a76b36b42294f99b4
OIdiotLin <lhd1997@qq.com>
2016-12-18 17:04:32 +0800

    UI

---
+++ src/main/java/com/oidiotlin/classmanager/activity/OverviewActivity.java
@@ -0,0 +1,13 @@
+package com.oidiotlin.classmanager.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OverviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_overview);
+    }
+}
