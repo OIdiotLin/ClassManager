7b5e4ea176e58123389a75bc0052e04e294895af
OIdiotLin <lhd1997@qq.com>
2016-12-19 11:20:30 +0800

    remove splash

--- src/main/java/com/oidiotlin/classmanager/activity/SplashActivity.java
+++ src/main/java/com/oidiotlin/classmanager/activity/SplashActivity.java
@@ -1,15 +1,35 @@
 package com.oidiotlin.classmanager.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.oidiotlin.classmanager.R;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends Activity {
+
+    private Handler mMainHandler = new Handler() {
+        public void handleMessage(Message message) {
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.setClass(getApplication(), OverviewActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+
+            finish();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
+        mMainHandler.sendEmptyMessageDelayed(0,3000);
     }
 }
--- src/main/java/com/oidiotlin/classmanager/view/CustomToolBar.java
+++ src/main/java/com/oidiotlin/classmanager/view/CustomToolBar.java
@@ -1,6 +1,15 @@
 package com.oidiotlin.classmanager.view;
 
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.oidiotlin.classmanager.R;
 
 /**
  * Created by OIdiot on 2016/12/18.
@@ -8,4 +17,70 @@ import android.widget.LinearLayout;
  */
 
 public class CustomToolBar extends LinearLayout {
+    private Boolean isLeftBtnVisible;
+    private Boolean isRightBtnVisible;
+    private Boolean isTitleVisible;
+    private int leftId;
+    private int rightId;
+    private String titleText;
+    private int backgroundId;
+
+
+    public CustomToolBar(Context context) {
+        this(context, null);
+    }
+
+    public CustomToolBar(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public CustomToolBar(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initView(attrs);
+    }
+
+    public void initView(AttributeSet attrs) {
+        TypedArray typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.CustomToolBar);
+        /**
+         * Access left button info
+         */
+        isLeftBtnVisible = typedArray.getBoolean(R.styleable.CustomToolBar_left_btn_visible, false);
+        leftId = typedArray.getResourceId(R.styleable.CustomToolBar_left_btn_src, -1);
+        /**
+         * Access right button info
+         */
+        isRightBtnVisible = typedArray.getBoolean(R.styleable.CustomToolBar_right_btn_visible, false);
+        rightId = typedArray.getResourceId(R.styleable.CustomToolBar_right_btn_src, -1);
+        /**
+         * Access title info
+         */
+        isTitleVisible = typedArray.getBoolean(R.styleable.CustomToolBar_title_visible, false);
+        if(typedArray.hasValue(R.styleable.CustomToolBar_title_text)) {
+            titleText = typedArray.getString(R.styleable.CustomToolBar_title_text);
+        }
+        /**
+         * Access background color
+         */
+        backgroundId = typedArray.getResourceId(R.styleable.CustomToolBar_bar_background, -1);
+        typedArray.recycle();
+
+        /**
+         * Set new properties
+         */
+        View barLayoutView = View.inflate(getContext(), R.layout.common_toolbar, null);
+        ImageButton leftBtn = (ImageButton)barLayoutView.findViewById(R.id.toolbar_left_btn);
+        ImageButton rightBtn = (ImageButton)barLayoutView.findViewById(R.id.toolbar_right_btn);
+        TextView title = (TextView)barLayoutView.findViewById(R.id.toolbar_title);
+        RelativeLayout bar = (RelativeLayout)barLayoutView.findViewById(R.id.layout_toolbar_content);
+
+        if(isLeftBtnVisible)    leftBtn.setVisibility(VISIBLE);
+        if(isRightBtnVisible)   rightBtn.setVisibility(VISIBLE);
+        if(isTitleVisible)      title.setVisibility(VISIBLE);
+
+        if(leftId != -1)    leftBtn.setBackgroundResource(leftId);
+        if(rightId != -1)   rightBtn.setBackgroundResource(rightId);
+        if(backgroundId != -1)  bar.setBackgroundColor(getResources().getColor(R.color.bg_toolbar));
+
+        addView(barLayoutView, 0);
+    }
 }
