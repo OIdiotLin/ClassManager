270774f761ad07674975ecb6c78dca83f4b87190
OIdiotLin <lhd1997@qq.com>
2016-12-19 11:20:03 +0800

    remove splash

---
+++ src/main/java/com/oidiotlin/classmanager/activity/ManagerActivity.java
@@ -0,0 +1,15 @@
+package com.oidiotlin.classmanager.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.oidiotlin.classmanager.R;
+
+public class ManagerActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_overview);
+    }
+}
---
+++ src/main/java/com/oidiotlin/classmanager/view/CustomToolBar.java
@@ -0,0 +1,11 @@
+package com.oidiotlin.classmanager.view;
+
+import android.widget.LinearLayout;
+
+/**
+ * Created by OIdiot on 2016/12/18.
+ * Project: ClassManager
+ */
+
+public class CustomToolBar extends LinearLayout {
+}
