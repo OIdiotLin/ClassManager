922abf9519f536835ea8b9468a407a3a0ff106c5
OIdiotLin <lhd1997@qq.com>
2017-02-05 13:55:06 +0800

    use try-catch in parse method

--- src/main/java/com/oidiotlin/classmanager/utils/AppInfoParser.java
+++ src/main/java/com/oidiotlin/classmanager/utils/AppInfoParser.java
@@ -24,35 +24,39 @@ public class AppInfoParser implements IAppInfoParser {
         XmlPullParser xmlPullParser = Xml.newPullParser();
         xmlPullParser.setInput(inputStream, "UTF-8");
 
-        int eventType = xmlPullParser.getEventType();   // get tag/event
+        try {
+            int eventType = xmlPullParser.getEventType();   // get tag/event
 
-        while (eventType != XmlPullParser.END_DOCUMENT) {
-            switch (eventType) {
-                // Event - Document's Starting.
-                case XmlPullParser.START_DOCUMENT:
-                    appInfoList = new ArrayList<AppInfo>();
-                    break;
-                // Event - Tag's Starting
-                case XmlPullParser.START_TAG:
-                    if (xmlPullParser.getName().equals(Constant.APPINFO))
-                        info = new AppInfo();
-                    else if (xmlPullParser.getName().equals(Constant.APPINFO_APPVER)) {
-                        eventType = xmlPullParser.next();
-                        info.setAppVer(Integer.parseInt(xmlPullParser.getText()));
-                    } else if (xmlPullParser.getName().equals(Constant.APPINFO_DBVER)) {
-                        eventType = xmlPullParser.next();
-                        info.setDbVer(Integer.parseInt(xmlPullParser.getText()));
-                    }
-                    break;
-                // Event - Tag's Ending.
-                case XmlPullParser.END_TAG:
-                    if (xmlPullParser.getName().equals(Constant.APPINFO)) {
-                        appInfoList.add(info);
-                        info = null;
-                    }
-                    break;
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                switch (eventType) {
+                    // Event - Document's Starting.
+                    case XmlPullParser.START_DOCUMENT:
+                        appInfoList = new ArrayList<AppInfo>();
+                        break;
+                    // Event - Tag's Starting
+                    case XmlPullParser.START_TAG:
+                        if (xmlPullParser.getName().equals(Constant.APPINFO))
+                            info = new AppInfo();
+                        else if (xmlPullParser.getName().equals(Constant.APPINFO_APPVER)) {
+                            eventType = xmlPullParser.next();
+                            info.setAppVer(Integer.parseInt(xmlPullParser.getText()));
+                        } else if (xmlPullParser.getName().equals(Constant.APPINFO_DBVER)) {
+                            eventType = xmlPullParser.next();
+                            info.setDbVer(Integer.parseInt(xmlPullParser.getText()));
+                        }
+                        break;
+                    // Event - Tag's Ending.
+                    case XmlPullParser.END_TAG:
+                        if (xmlPullParser.getName().equals(Constant.APPINFO)) {
+                            appInfoList.add(info);
+                            info = null;
+                        }
+                        break;
+                }
+                eventType = xmlPullParser.next();
             }
-            eventType = xmlPullParser.next();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
         return appInfoList;
     }
