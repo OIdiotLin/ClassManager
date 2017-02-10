e810d2a4c46f381dcdd82e2e7b69a8d15c8315ac
OIdiotLin <lhd1997@qq.com>
2017-02-11 00:13:48 +0800

    add interfaces to Parser

--- app/src/main/java/com/oidiotlin/classmanager/utils/network/IUrlParser.java
+++ app/src/main/java/com/oidiotlin/classmanager/utils/network/IUrlParser.java
@@ -1,9 +1,28 @@
 package com.oidiotlin.classmanager.utils.network;
 
+import java.io.InputStream;
+import java.net.URL;
+import java.util.List;
+
 /**
  * Created by OIdiot on 2017/2/10.
  * Project: ClassManager
  */
 
 public interface IUrlParser {
+    /**
+     * parse inputStream, access the url list
+     * @param inputStream
+     * @return
+     * @throws Exception
+     */
+    public List<URL> parse(InputStream inputStream) throws Exception;
+
+    /**
+     * serialize object set - URL, access xml in String format
+     * @param urls
+     * @return
+     * @throws Exception
+     */
+    public String serialize(List<URL> urls) throws Exception;
 }
--- app/src/main/java/com/oidiotlin/classmanager/utils/network/UrlParser.java
+++ app/src/main/java/com/oidiotlin/classmanager/utils/network/UrlParser.java
@@ -1,9 +1,24 @@
 package com.oidiotlin.classmanager.utils.network;
 
+import java.io.InputStream;
+import java.net.URL;
+import java.util.List;
+
 /**
  * Created by OIdiot on 2017/2/10.
  * Project: ClassManager
  */
 
 public class UrlParser implements IUrlParser {
+    @Override
+    public List<URL> parse(InputStream inputStream) throws Exception {
+        // TODO Implement parse method
+        return null;
+    }
+
+    @Override
+    public String serialize(List<URL> urls) throws Exception {
+        // TODO Implement serialize method
+        return null;
+    }
 }
