1c6a7796f0324574622f9be5224ad5b864ccd060
OIdiotLin <lhd1997@qq.com>
2017-02-10 23:55:03 +0800

    finish CheckVersionTask

--- app/src/main/java/com/oidiotlin/classmanager/utils/network/CheckVersionTask.java
+++ app/src/main/java/com/oidiotlin/classmanager/utils/network/CheckVersionTask.java
@@ -11,8 +11,14 @@ import java.net.URL;
 
 import static com.oidiotlin.classmanager.utils.system.AppUtils.getVersionCode;
 import static com.oidiotlin.classmanager.utils.system.Constant.API_CHECKUPDATE;
+import static com.oidiotlin.classmanager.utils.system.Constant.FORCED_UPDATE;
+import static com.oidiotlin.classmanager.utils.system.Constant.NO_UPDATE;
+import static com.oidiotlin.classmanager.utils.system.Constant.OPTIONAL_UPDATE;
 import static com.oidiotlin.classmanager.utils.system.Constant.SERVER_API;
+import static com.oidiotlin.classmanager.utils.system.Constant.SERVER_RESPONSE_TIMEOUT;
 import static com.oidiotlin.classmanager.utils.system.Constant.UPDATE_CLIENT;
+import static com.oidiotlin.classmanager.utils.system.Constant.UPDATE_ERROR;
+import static com.oidiotlin.classmanager.utils.system.Constant.VERSION_MAX_DIFF_TOLERANCE;
 
 /**
  * Created by OIdiot on 2017/2/9.
@@ -23,7 +29,6 @@ public class CheckVersionTask implements Runnable {
     private static final String TAG = "CheckVersionTask";
     private Context context = null;
     private Handler handler;
-    private AppInfo remoteInfo;
 
     public CheckVersionTask(Context context, Handler handler) {
         this.context = context;
@@ -35,18 +40,34 @@ public class CheckVersionTask implements Runnable {
         try {
             URL url = new URL(SERVER_API + API_CHECKUPDATE);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setConnectTimeout(4000);
+            connection.setConnectTimeout(SERVER_RESPONSE_TIMEOUT);
             InputStream inputStream = connection.getInputStream();
-            remoteInfo = (new AppInfoParser()).parse(inputStream).get(0);
+            AppInfo remoteInfo = (new AppInfoParser()).parse(inputStream).get(0);
 
             int remoteVersion = remoteInfo.getVersionCode();
             int localVersion = getVersionCode(context);
             Log.i(TAG, "run: remoteVer = " + remoteVersion + "localVer = " + localVersion);
 
+            if (remoteVersion - localVersion > VERSION_MAX_DIFF_TOLERANCE) {    // Forced Update
+                Message msg = new Message();
+                msg.what = UPDATE_CLIENT;
+                msg.arg1 = FORCED_UPDATE;
+                handler.sendMessage(msg);
+            } else if (remoteVersion > localVersion) {      // Choose to update or not
+                Message msg = new Message();
+                msg.what = UPDATE_CLIENT;
+                msg.arg1 = OPTIONAL_UPDATE;
+                handler.sendMessage(msg);
+            } else {
+                Message msg = new Message();
+                msg.what = UPDATE_CLIENT;
+                msg.arg1 = NO_UPDATE;
+                handler.sendMessage(msg);
+            }
+
         } catch (Exception e) {
             Message msg = new Message();
-            msg.what = UPDATE_CLIENT;
-            msg.obj = remoteInfo.getNewFeatures();
+            msg.what = UPDATE_ERROR;
             handler.sendMessage(msg);
             e.printStackTrace();
         }
