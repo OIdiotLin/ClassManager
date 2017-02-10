a89d601764c34926125ac025823857314823d1b2
OIdiotLin <lhd1997@qq.com>
2017-02-10 22:57:45 +0800

    create UpdateAppTask implements Runnable

---
+++ app/src/main/java/com/oidiotlin/classmanager/utils/network/UpdateAppTask.java
@@ -0,0 +1,20 @@
+package com.oidiotlin.classmanager.utils.network;
+
+import android.content.Context;
+
+/**
+ * Created by OIdiot on 2017/2/10.
+ * Project: ClassManager
+ */
+
+public class UpdateAppTask implements Runnable {
+    private Context context;
+    public UpdateAppTask(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
