8a2a0595aeb75b23728a6072de7320b1a8bec5f5
OIdiotLin <lhd1997@qq.com>
2017-02-10 22:55:48 +0800

    finish UpdateDialog

--- app/src/main/java/com/oidiotlin/classmanager/view/UpdateDialog.java
+++ app/src/main/java/com/oidiotlin/classmanager/view/UpdateDialog.java
@@ -2,8 +2,17 @@ package com.oidiotlin.classmanager.view;
 
 import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.oidiotlin.classmanager.R;
+import com.oidiotlin.classmanager.activity.MainActivity;
 import com.oidiotlin.classmanager.utils.network.AppInfo;
+import com.oidiotlin.classmanager.utils.network.UpdateAppTask;
 
 /**
  * Created by OIdiot on 2017/2/10.
@@ -11,8 +20,33 @@ import com.oidiotlin.classmanager.utils.network.AppInfo;
  */
 
 public class UpdateDialog extends AlertDialog.Builder {
-
-    public UpdateDialog(Context context, AppInfo info) {
+    public UpdateDialog(final Context context, AppInfo info) {
         super(context);
+        final View dialogView = LayoutInflater.from(context).inflate(R.layout.dialog_update, null);
+        this.setIcon(R.drawable.ic_speaker);
+        this.setTitle(R.string.update_dialog_title);
+        this.setView(dialogView);
+        TextView textView = (TextView) dialogView.findViewById(R.id.update_new_features);
+        textView.setText(info.getNewFeatures());
+
+        this.setPositiveButton(R.string.update_yes,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // TODO Download new APK and install
+                        Toast.makeText(context,
+                                R.string.update_start_hint, Toast.LENGTH_SHORT).show();
+                        new UpdateAppTask(context).run();
+                    }
+                });
+        this.setNegativeButton(R.string.update_no,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(context, MainActivity.class);
+                        context.startActivity(intent);
+                    }
+                });
+
     }
 }
