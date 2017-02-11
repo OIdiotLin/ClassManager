774b5ae1fca961a5637559063bdae0275a2f684d
OIdiotLin <lhd1997@qq.com>
2017-02-11 14:58:15 +0800

    finish serialize method

--- app/src/main/java/com/oidiotlin/classmanager/utils/network/UrlParser.java
+++ app/src/main/java/com/oidiotlin/classmanager/utils/network/UrlParser.java
@@ -3,13 +3,17 @@ package com.oidiotlin.classmanager.utils.network;
 import android.util.Xml;
 
 import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
+import org.xmlpull.v1.XmlSerializer;
 
 import java.io.InputStream;
+import java.io.StringWriter;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
 import static com.oidiotlin.classmanager.utils.system.Constant.SERVER_RETURN_URL_NODE;
+import static com.oidiotlin.classmanager.utils.system.Constant.XML_ROOT;
 
 /**
  * Created by OIdiot on 2017/2/10.
@@ -19,7 +23,6 @@ import static com.oidiotlin.classmanager.utils.system.Constant.SERVER_RETURN_URL
 public class UrlParser implements IUrlParser {
     @Override
     public List<URL> parse(InputStream inputStream) throws Exception {
-        // TODO Implement parse method
         List<URL> urls = null;
         URL url;
         XmlPullParser xmlPullParser = Xml.newPullParser();
@@ -53,6 +56,25 @@ public class UrlParser implements IUrlParser {
     @Override
     public String serialize(List<URL> urls) throws Exception {
         // TODO Implement serialize method
-        return null;
+        StringWriter stringWriter = new StringWriter();
+        try {
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            XmlSerializer xmlSerializer = factory.newSerializer();
+            xmlSerializer.setOutput(stringWriter);
+            xmlSerializer.startDocument("UTF-8", true);
+
+            xmlSerializer.startTag(null, XML_ROOT);
+            for (URL url : urls) {
+                xmlSerializer.startTag(null, SERVER_RETURN_URL_NODE);
+                xmlSerializer.text(String.valueOf(url.getPath()));
+                xmlSerializer.endTag(null, SERVER_RETURN_URL_NODE);
+            }
+            xmlSerializer.endTag(null, XML_ROOT);
+
+            xmlSerializer.endDocument();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return stringWriter.toString();
     }
 }
