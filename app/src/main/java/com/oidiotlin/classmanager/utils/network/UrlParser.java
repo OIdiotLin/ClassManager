72838e9f8312df038c01373a44e4697309ba664e
OIdiotLin <lhd1997@qq.com>
2017-02-11 14:40:54 +0800

    finish parse method

--- app/src/main/java/com/oidiotlin/classmanager/utils/network/UrlParser.java
+++ app/src/main/java/com/oidiotlin/classmanager/utils/network/UrlParser.java
@@ -1,8 +1,15 @@
 package com.oidiotlin.classmanager.utils.network;
 
+import android.util.Xml;
+
+import org.xmlpull.v1.XmlPullParser;
+
 import java.io.InputStream;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
+
+import static com.oidiotlin.classmanager.utils.system.Constant.SERVER_RETURN_URL_NODE;
 
 /**
  * Created by OIdiot on 2017/2/10.
@@ -13,7 +20,34 @@ public class UrlParser implements IUrlParser {
     @Override
     public List<URL> parse(InputStream inputStream) throws Exception {
         // TODO Implement parse method
-        return null;
+        List<URL> urls = null;
+        URL url;
+        XmlPullParser xmlPullParser = Xml.newPullParser();
+        xmlPullParser.setInput(inputStream, "UTF-8");
+
+        try {
+            int eventType = xmlPullParser.getEventType();
+
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                switch (eventType) {
+                    case XmlPullParser.START_DOCUMENT:
+                        urls = new ArrayList<>();
+                        break;
+                    case XmlPullParser.START_TAG:
+                        // nothing here
+                        if (xmlPullParser.getName().equals(SERVER_RETURN_URL_NODE)) {
+                            eventType = xmlPullParser.next();
+                            url = new URL(xmlPullParser.getText());
+                            urls.add(url);
+                            url = null;
+                        }
+                }
+                eventType = xmlPullParser.next();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return urls;
     }
 
     @Override
