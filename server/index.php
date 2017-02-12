5729995c519e9f1b90db3c0392c7ec4d505b12cd
OIdiotLin <lhd1997@qq.com>
2017-02-12 11:46:55 +0800

    create index.php on server

---
+++ server/index.php
@@ -0,0 +1,20 @@
+<?php
+
+echo "
+<html>
+<head>
+	<title>ClassManager Server</title>
+</head>
+
+<body>
+	<h1 style='text-align:center'>This is the server of app ClassManager</h1>
+	<hr />
+	<p>
+		Please contact with H.D. Lin.
+	</p>
+</body>
+
+</html>
+";
+
+?>
