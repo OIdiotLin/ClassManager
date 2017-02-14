833ec555bbd12564b569b3611672906a1b803227
OIdiotLin <lhd1997@qq.com>
2017-02-14 23:31:21 +0800

    add class AppVersionInfo

---
+++ server/applications/AppVersionInfo.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+* app 版本信息
+*/
+class AppVersionInfo {
+
+	private $versionCode;	// 版本号
+	private $versionName;	// 版本名
+	private $newFeatures;	// 新特性
+
+	function __construct($versionCode, $versionName, $newFeatures) {
+		$this->versionCode = $versionCode;
+		$this->versionName = $versionName;
+		$this->newFeatures = $newFeatures;
+	}
+
+	public function setVersionCode($versionCode) {
+		$this->versionCode = $versionCode;
+	}
+
+	public function getVersionCode() {
+		return $versionCode;
+	}
+
+	public function setVersionName($versionName) {
+		$this->versionName = $versionName;
+	}
+
+	public function getVersionName() {
+		return $versionName;
+	}
+
+	public function setNewFeatures($newFeatures) {
+		$this->newFeatures = $newFeatures;
+	}
+
+	public function getNewFeatures() {
+		return $newFeatures;
+	}
+}
+
+?>
