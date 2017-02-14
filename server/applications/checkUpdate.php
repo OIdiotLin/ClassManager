c36ee642780d3cafff2db40b5b2fcdec18461716
OIdiotLin <lhd1997@qq.com>
2017-02-14 23:50:18 +0800

    finish checkUpdate function

--- server/applications/checkUpdate.php
+++ server/applications/checkUpdate.php
@@ -1,5 +1,11 @@
 <?php
 
+require_once 'utils/response.php';
+require_once 'AppVersionInfo.php';
 
+function askUpdate() {
+	$info = new AppVersionInfo(UPDATE_VERSION_CODE, UPDATE_VERSION_NAME, UPDATE_NEW_FEATURES);
+	Response::show(200, 'OK', $info->toArray());
+}
 
 ?>
