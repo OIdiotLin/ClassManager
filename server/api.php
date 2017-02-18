9507a638e52ebb82ddf886a0edeb43200b1cfad8
OIdiotLin <lhd1997@qq.com>
2017-02-18 18:53:30 +0800

    add api 'API_GET_PERSON_LIST'

--- server/api.php
+++ server/api.php
@@ -1,17 +1,33 @@
 <?php
 
 require_once 'applications/update.php';
+require_once 'applications/personManage.php';
 require_once 'utils/const.php';
 require_once 'index.php';
 
 function business($act) {
 	switch ($act) {
+		/**
+		 * @return latest app version infomation
+		 */
 		case API_CHECK_UPDATE:
-			askUpdate();	// return new version
+			askUpdate();
 			break;
+		/**
+		 * @return url of new apk for updating
+		 */
 		case API_GET_APK_URL:
-			getApkUrl();	// return new apk file url
+			getApkUrl();
 			break;
+		/**
+		 * @return list of persons
+		 */
+		case API_GET_PERSON_LIST:
+			getPersonList();	
+			break;
+		/**
+		 * show index if without any recognized args
+		 */
 		default:
 			show_index();
 			break;
