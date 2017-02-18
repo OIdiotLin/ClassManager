952cc86f54d7688ca56f49d60562943701131ba0
OIdiotLin <lhd1997@qq.com>
2017-02-18 21:24:12 +0800

    finish getPersonList() and return XML successfully.

--- server/applications/personManage.php
+++ server/applications/personManage.php
@@ -3,14 +3,24 @@
 require_once 'Person.php';
 
 function getPersonList() {
-	$connection = mysqli_connect(DB_HOST, DB_USER, DB_PWD);
+	$connection = mysql_connect(DB_HOST, DB_USER, DB_PWD);
 	if ($connection == null) {
 		die('Could not connect: '.DB_HOST);
 	}
 	mysql_select_db(DB_NAME, $connection);
+	mysql_query("SET NAMES UTF8");
+
 	$sql = "SELECT * from ".TBL_PERSONS." WHERE true";
-	$result = mysql_fetch_assoc(mysql_query($sql, $connection));
-	var_dump($result);
+	$result = mysql_query($sql);
+
+	$list = array();
+
+	while ($info = mysql_fetch_assoc($result)) {
+		$person = array('person' => $info);
+		array_push($list, $person);
+	}
+
+	Response::show(200, 'OK', $list);
 }
 
 ?>
