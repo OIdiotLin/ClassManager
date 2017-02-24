4d10cce778f4cccb70bf7062050de807390b1cf1
OIdiotLin <lhd1997@qq.com>
2017-02-24 17:46:04 +0800

    create FormItem.java

---
+++ app/src/main/java/com/oidiotlin/classmanager/view/FormItem.java
@@ -0,0 +1,16 @@
+package com.oidiotlin.classmanager.view;
+
+import android.view.View;
+import android.view.View.OnClickListener;
+
+/**
+ * Created by OIdiot on 2017/2/24.
+ * Project: ClassManager
+ */
+
+public class FormItem implements OnClickListener {
+    @Override
+    public void onClick(View v) {
+         
+    }
+}
