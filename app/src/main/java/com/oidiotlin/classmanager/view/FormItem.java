9050192b9c4b66b901a49d2bcfa69b3851c267dc
OIdiotLin <lhd1997@qq.com>
2017-02-25 01:37:56 +0800

    create new View - FormItem

--- app/src/main/java/com/oidiotlin/classmanager/view/FormItem.java
+++ app/src/main/java/com/oidiotlin/classmanager/view/FormItem.java
@@ -1,16 +1,48 @@
 package com.oidiotlin.classmanager.view;
 
-import android.view.View;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
 import android.view.View.OnClickListener;
+import android.widget.LinearLayout;
+
+import com.oidiotlin.classmanager.R;
 
 /**
  * Created by OIdiot on 2017/2/24.
  * Project: ClassManager
  */
 
-public class FormItem implements OnClickListener {
-    @Override
-    public void onClick(View v) {
-         
+public class FormItem extends LinearLayout implements OnClickListener {
+    private Boolean isTitleVisible;
+    private int titleId;
+    private int contentId;
+    private int backgroundId;
+
+    public FormItem(Context context) {
+        this(context, null);
+    }
+
+    public FormItem(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public FormItem(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initView(attrs);
+    }
+
+    public void initView(AttributeSet attrs) {
+        TypedArray typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.FormItem);
+        /**
+         * Access title TextView
+         */
+        isTitleVisible = typedArray.getBoolean(R.styleable.FormItem_title_visible, false);
+        titleId = typedArray.getResourceId(R.styleable.FormItem_title_text, -1);
+        /**
+         * Access contentId
+         */
+        // TODO 获取其他属性
+        typedArray.recycle();
     }
 }
