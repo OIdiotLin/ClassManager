e45a55871c7b9edbea922c17bee23a1c7b9ba187
OIdiotLin <lhd1997@qq.com>
2017-03-10 23:33:41 +0800

    invoke GetEventListTask and handle msg from it to refresh UI

--- app/src/main/java/com/oidiotlin/classmanager/fragment/EventFragment.java
+++ app/src/main/java/com/oidiotlin/classmanager/fragment/EventFragment.java
@@ -1,6 +1,8 @@
 package com.oidiotlin.classmanager.fragment;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -9,11 +11,17 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.oidiotlin.classmanager.R;
+import com.oidiotlin.classmanager.utils.network.GetEventListTask;
 import com.oidiotlin.classmanager.utils.parser.Event;
 
 import java.util.List;
+
+import static com.oidiotlin.classmanager.utils.system.Constant.GET_EVENTS_LIST_TASK;
+import static com.oidiotlin.classmanager.utils.system.Constant.GET_EVENTS_LIST_TASK_FAIL;
+import static com.oidiotlin.classmanager.utils.system.Constant.GET_EVENTS_LIST_TASK_SUCCESS;
 
 /**
  * Created by OIdiot on 2016/12/21.
@@ -33,7 +41,7 @@ public class EventFragment extends Fragment {
         Log.i(TAG, "onCreateView: ");
         listView = (ListView) view.findViewById(R.id.events_list);
 
-        listView.setAdapter(new EventAdapter());
+        new Thread(new GetEventListTask(getActivity(), handler)).start();
 
         return view;
     }
@@ -55,6 +63,7 @@ public class EventFragment extends Fragment {
             /**
              * Access the Views
              */
+            //TODO 绑定数据
             return v;
         }
 
@@ -70,4 +79,28 @@ public class EventFragment extends Fragment {
             return null;
         }
     }
+
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                /** 获取 events 表 **/
+                case GET_EVENTS_LIST_TASK:
+                    switch (msg.arg1) {
+                        /** 获取成功 **/
+                        case GET_EVENTS_LIST_TASK_SUCCESS:
+                            listView.setAdapter(new EventAdapter());
+                            break;
+                        /** 获取失败 **/
+                        case GET_EVENTS_LIST_TASK_FAIL:
+                            Toast.makeText(getActivity(),
+                                    R.string.info_connecting_err, Toast.LENGTH_SHORT).show();
+
+                            break;
+                    }
+                    break;
+            }
+        }
+    };
 }
